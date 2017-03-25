13596b28d982adddfcde29af829ab16326a6da57
stefanbuetler <stefan.buetler@gmail.com>
2017-03-25 09:34:07 +0100

    upload to github

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 function showMessage() {
-  alert('Hallo Welt');
+  console.log('Hallo Entwicklerwelt');
+  console.trace();
+  console.debug('DEB');
+  console.error('ERR');
+  console.info('INFO');
 }
+function calculateSum() {
+  var x = parseInt(document.getElementById('field1').value);
+  var y = parseInt(document.getElementById('field2').value);
+  var result = document.getElementById('result');
+  console.log(x + y);
+  result.value = x + y;
+}
+calculateSum();
 showMessage();
